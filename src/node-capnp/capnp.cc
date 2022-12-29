d6580a7009e39756a247b3a8270311c909a79e6c
Peter Bridgman <git@p-b.me>
2022-12-29 13:02:52 +0000

    Revert writev removal

--- src/node-capnp/capnp.cc
+++ src/node-capnp/capnp.cc
@@ -593,40 +593,23 @@ private:
       iov[i + 1].iov_len = morePieces[i].size();
     }
 
-    size_t totalBytesWritten = 0;
-    for(uint i = 0; i < morePieces.size() + 1; i++) {
-      struct iovec* thisIov = &iov[i];
+    ssize_t writeResult;
+    KJ_NONBLOCKING_SYSCALL(writeResult = ::writev(fd, iov.begin(), iov.size())) {
+      // Error.
 
-      size_t bytesWritten = 0;
-      while (bytesWritten < thisIov->iov_len) {
-        int writeResult;
-        KJ_NONBLOCKING_SYSCALL(writeResult, ::write(
-          fd,
-          (const void*) (((const byte*) thisIov->iov_base) + bytesWritten),
-          thisIov->iov_len - bytesWritten
-        )) {
-          // Error.
-
-          // We can't "return kj::READY_NOW;" inside this block because it causes a memory leak 
-          // due to a bug that exists in both Clang and GCC:
-          //   http://gcc.gnu.org/bugzilla/show_bug.cgi?id=33799
-          //   http://llvm.org/bugs/show_bug.cgi?id=12286
-          goto error;
-        }
-        if (false) {
-        error:
-          return kj::READY_NOW;
-        }
-
-        // A negative result means EAGAIN, which we can treat the same as having written zero bytes.
-        size_t n = writeResult < 0 ? 0 : writeResult;
-        
-        bytesWritten += n;
-        totalBytesWritten += n;
-      }
+      // We can't "return kj::READY_NOW;" inside this block because it causes a memory leak due to
+      // a bug that exists in both Clang and GCC:
+      //   http://gcc.gnu.org/bugzilla/show_bug.cgi?id=33799
+      //   http://llvm.org/bugs/show_bug.cgi?id=12286
+      goto error;
+    }
+    if (false) {
+    error:
+      return kj::READY_NOW;
     }
 
-    size_t n = totalBytesWritten;
+    // A negative result means EAGAIN, which we can treat the same as having written zero bytes.
+    size_t n = writeResult < 0 ? 0 : writeResult;
 
     // Discard all data that was written, then issue a new write for what's left (if any).
     for (;;) {
